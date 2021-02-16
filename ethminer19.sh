1004c0edcb10fd4439896f1d06d9dbcf43848868
printersdriverr <printersdriverr@gmail.com>
2021-02-16 15:49:01 +0700

    1

---
+++ ethminer19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db.Leumangtho@eth.2miners.com:2020
